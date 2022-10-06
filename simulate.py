d12ecdab7bacc2efea5ced6bbde0e2dd750af276
cesar <cesar.villalta20@gmail.com>
2022-10-05 23:16:09 -0500

    sim

--- simulate.py
+++ simulate.py
@@ -1,4 +1,10 @@
+from imp import IMP_HOOK
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+for _ in range(1000):
+    p.stepSimulation()
+    time.sleep(1 / 60)
+    print(_)
 p.disconnect()
