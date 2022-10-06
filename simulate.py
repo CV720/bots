ed5d662952de70b2b2e7ff64e01b8137bdef79d8
cesar <cesar.villalta20@gmail.com>
2022-10-06 01:23:34 -0500

    floor

--- simulate.py
+++ simulate.py
@@ -1,8 +1,12 @@
-from imp import IMP_HOOK
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0, 0, -9.8)
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 for _ in range(1000):
     p.stepSimulation()
