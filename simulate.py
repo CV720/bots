67005a455db5efb643e8b69bdf9a481b968e2fb6
cesar <cesar.villalta20@gmail.com>
2022-12-25 11:16:45 -0600

    new

--- simulate.py
+++ simulate.py
@@ -15,14 +15,15 @@ p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValue = np.zeros(1000)
-for _ in range(1000):
+for _ in range(1_000):
     p.stepSimulation()
     backLegSensorValues[_] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValue[_] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b'Torso_BackLeg', controlMode=p.POSITION_CONTROL, targetPosition=-np.pi/4, maxForce=500)
+    # pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b'Torso_FrontLeg', controlMode=p.POSITION_CONTROL, targetPosition=np.pi/2, maxForce=500)
     if _ % 100 == 0:
         print(_)
     time.sleep(1 / 60)
 np.save("backLegSensorValues", backLegSensorValues)
 np.save("data/FrontLegSensorValues", frontLegSensorValue)
-print("hello")
 p.disconnect()
