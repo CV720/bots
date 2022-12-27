1ae9232387854aa1d2b7dbaa0225de963b0799a8
cesar <cesar.villalta20@gmail.com>
2022-12-27 08:05:05 -0600

    clean

--- motor.py
+++ motor.py
@@ -8,13 +8,12 @@ class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
         self.Prepare_To_Act()
-        self.motorValues = self.amplitude * np.sin(self.frequency * np.linspace(0, 2 * np.pi, 1000) + self.offset)
 
     def Prepare_To_Act(self):
-        print(self.jointName)
         self.amplitude = c.Amplitude if self.jointName == b'Torso_BackLeg' else c.Amplitude / 2
         self.frequency = c.Frequency
         self.offset = c.PhaseOffset
+        self.motorValues = self.amplitude * np.sin(self.frequency * np.linspace(0, 2 * np.pi, 1000) + self.offset)
 
     def Set_Value(self, t, robotId):
         pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=self.jointName, controlMode=p.POSITION_CONTROL,
