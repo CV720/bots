face88c90e08bd507ad1a826e67dd6c97d4252cd
Cesar <cesar.villalta20@gmail.com>
2022-12-27 17:14:14 -0600

    nnfinal

--- motor.py
+++ motor.py
@@ -8,17 +8,9 @@ import pyrosim.pyrosim as pyrosim
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
-        self.Prepare_To_Act()
 
-    def Prepare_To_Act(self):
-        self.amplitude = c.Amplitude if self.jointName == b'Torso_BackLeg' else c.Amplitude / 2
-        self.frequency = c.Frequency
-        self.offset = c.PhaseOffset
-        self.motorValues = self.amplitude * np.sin(self.frequency * np.linspace(0, 2 * np.pi, 1000) + self.offset)
-
-    def Set_Value(self, t, robotId):
+    def Set_Value(self, desiredAngle, robotId):
         pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=self.jointName, controlMode=p.POSITION_CONTROL,
-                                    targetPosition=self.motorValues[t], maxForce=50)
+                                    targetPosition=desiredAngle, maxForce=50)
 
-    def Save_Value(self):
-        np.save("data/motorvalues" + self.jointName, self.motorValues)
+
--- robot.py
+++ robot.py
@@ -30,8 +30,16 @@ class ROBOT:
             sensor.Get_Value(t)
 
     def Act(self, t):
-        for motor in self.motors.values():
-            motor.Set_Value(t, self.robotId)
+        for neuronName in self.nn.Get_Neuron_Names():
+            if self.nn.Is_Motor_Neuron(neuronName):
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+                desireAngle = self.nn.Get_Value_Of(neuronName)
+                self.motors[bytes(jointName, 'utf-8')].Set_Value(desireAngle, self.robotId)
+
+        #
+        # for motor in self.motors.values():
+        #     motor.Set_Value(t, self.robotId)
 
     def Think(self):
+        self.nn.Update()
         self.nn.Print()
